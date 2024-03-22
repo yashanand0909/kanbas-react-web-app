dad6b524d5026c39a87f37b4a476150364ae5561
Yash Anand <yashanand.0909@gmail.com>
2024-03-21 23:27:34 -0400

    corrected todo buttons

--- src/Labs/a4/ReduxExamples/todos/TodoForm.tsx
+++ src/Labs/a4/ReduxExamples/todos/TodoForm.tsx
@@ -8,8 +8,8 @@ function TodoForm() {
     const dispatch = useDispatch();
     return (
       <li className="list-group-item">
-        <button className="btn btn-success" onClick={() => dispatch(addTodo(todo))}> Add </button>
-        <button className="btn btn-warning" onClick={() => dispatch(updateTodo(todo))}> Update </button>
+        <button className="btn btn-success float-end me-2" onClick={() => dispatch(addTodo(todo))}> Add </button>
+        <button className="btn btn-warning float-end me-2" onClick={() => dispatch(updateTodo(todo))}> Update </button>
         <input
           value={todo.title}
           onChange={ (e) => dispatch(setTodo({ ...todo, title: e.target.value }))}
--- src/Labs/a4/ReduxExamples/todos/TodoItem.tsx
+++ src/Labs/a4/ReduxExamples/todos/TodoItem.tsx
@@ -6,8 +6,8 @@ function TodoItem({todo} : {todo: { id: string; title: string } }) {
   const dispatch = useDispatch();
   return (
     <li key={todo.id} className="list-group-item">
-      <button className="btn btn-danger" onClick={() => dispatch(deleteTodo(todo.id))}> Delete </button>
-      <button className="btn btn-secondary" onClick={() => dispatch(setTodo(todo))}> Edit </button>
+      <button className="btn btn-danger float-end me-2" onClick={() => dispatch(deleteTodo(todo.id))}> Delete </button>
+      <button className="btn btn-secondary float-end me-2" onClick={() => dispatch(setTodo(todo))}> Edit </button>
       {todo.title}
     </li>
   );
